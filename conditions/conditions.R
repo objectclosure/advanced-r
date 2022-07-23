38246ccf9bb65978b2410b7be4bc4885b46e41b5
objectclosure <objectoftypeclosure@gmail.com>
2022-07-23 16:27:38 +0300

    warnings & messages

--- conditions/conditions.R
+++ conditions/conditions.R
@@ -23,3 +23,37 @@ h <- function() abort("This is an error")
 f()
 
 
+# warnings----
+fw <- function() {
+  cat("1\n")
+  warning("W1")
+  cat("2\n")
+  warning("W2")
+  cat("3\n")
+  warning("W3")
+}
+
+fw()
+# by default warnings appear after control returns to top level
+
+# to make warnings appear immediately:
+options(warn = 1)
+fw()
+
+# to convert warnings to errors:
+options(warn = 2)
+fw()
+
+# restore default behaviour:
+options(warn = 0)
+fw()
+
+# messages----
+fm <- function() {
+  for (i in seq_len(3)) {
+    cat(i, "\n")
+    message("M", i)
+  }
+}
+
+fm()
